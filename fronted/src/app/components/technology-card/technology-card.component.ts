7c64a0a52433b8002842d4984fa3b3056eb9d253
Angie Cohen <angiedev2001@gmail.com>
2024-01-15 19:07:18 -0300

    inicio de la construccion del frontend

---
+++ fronted/src/app/components/technology-card/technology-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-technology-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './technology-card.component.html',
+  styleUrl: './technology-card.component.css'
+})
+export class TechnologyCardComponent {
+
+}
